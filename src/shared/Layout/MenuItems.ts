97eae1cf66e488300d4866f8a6281dc9447424ed
Konstantin Zaytcev <zaydisk@yandex.ru>
2025-05-11 17:40:40 +0300

    removed unused menu items

--- src/shared/Layout/MenuItems.ts
+++ src/shared/Layout/MenuItems.ts
@@ -1,23 +1,23 @@
 import {MenuItem} from '@gravity-ui/navigation/build/esm/components/types';
-import {Briefcase, CircleExclamation, House} from '@gravity-ui/icons';
+import {Briefcase} from '@gravity-ui/icons';
 
 export const getMenuItems = (navigate: (path: string) => void): MenuItem[] => [
-    {
-        id: 'main',
-        icon: House,
-        title: 'Главная',
-        onItemClick: () => navigate('/'),
-    },
+    // {
+    //     id: 'main',
+    //     icon: House,
+    //     title: 'Главная',
+    //     onItemClick: () => navigate('/'),
+    // },
     {
         id: 'projects',
         icon: Briefcase,
         title: 'Проекты',
         onItemClick: () => navigate('/projects'),
     },
-    {
-        id: 'logs',
-        icon: CircleExclamation,
-        title: 'Логи',
-        onItemClick: () => navigate('/logs'),
-    },
+    // {
+    //     id: 'logs',
+    //     icon: CircleExclamation,
+    //     title: 'Логи',
+    //     onItemClick: () => navigate('/logs'),
+    // },
 ];
