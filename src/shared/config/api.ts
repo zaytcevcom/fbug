a2e5ccb2cafc0333ac7ffaaa83a53c870695a5f8
Konstantin Zaytcev <zaydisk@yandex.ru>
2025-05-25 22:53:40 +0300

    changed API_BASE_URL

--- src/shared/config/api.ts
+++ src/shared/config/api.ts
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = 'https://api.fuckbug.io/v1';
-export const API_BASE_URL = 'http://localhost/v1';
+export const API_BASE_URL = 'https://api.fuckbug.io/v1';
+// export const API_BASE_URL = 'http://localhost/v1';
